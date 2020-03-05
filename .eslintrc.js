e4359a3d82fc47f1e2555798230d7026db198fe8
Robin Hartmann <contact.robin.hartmann@gmail.com>
2020-03-06 00:36:54 +0100

    Re-order eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,9 +27,9 @@ module.exports = {
     sourceType: 'script'
   },
   rules: {
-    strict: ['error', 'global'],
     // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
     'no-prototype-builtins': 'off',
+    strict: ['error', 'global'],
     // https://github.com/basarat/typescript-book/blob/master/docs/tips/defaultIsBad.md
     'import/prefer-default-export': 'off',
     'import/no-default-export': 'error',
